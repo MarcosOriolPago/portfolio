945f2b9c030973902e3d31b15a2b3169447c2bc0
MarcosOriolPago <marcos.oriol.p@gmail.com>
2025-04-08 01:13:39 +0200

    feat: start cachiao

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from '../ProjectCard';
 import SkillIconGroup from '../SkillIconGroup';
 import { 
     SiJavascript, SiReact, SiTailwindcss, SiHtml5, SiPhp, SiMysql, SiTensorflow, 
-    SiPython, SiC, SiPandas, SiNumpy, SiGit, SiPytorch, SiVite,
+    SiPython, SiC, SiArduino, SiNumpy, SiGit, SiPytorch, SiVite,
     SiDocker
   } from 'react-icons/si';
 
@@ -12,7 +12,7 @@ const Projects = () => {
     return (
         <section id="projects" className="py-10">
         <div className="px-4">
-            <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            <h2 className="text-3xl font-bold mb-20 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                 {" "}
                 Projects
             </h2>
@@ -31,7 +31,7 @@ const Projects = () => {
                         </SkillIconGroup>
                     }
                     detailedDescription={
-                        <div className="overflow-y-scroll scrollbar-hidden h-120">
+                        <div className="overflow-y-scroll h-120">
                             <p className="text-gray-400 mt-2 mb-4">
                                 This project implements a Spiking Neural Network (SNN) model for detecting ripples in neural signals. 
                                 The SNN is designed to run on low-power microcontrollers, making it suitable for real-time applications in closed-loop systems.
@@ -78,17 +78,27 @@ const Projects = () => {
                 />
                 <ProjectCard
                     title="Rehabilitation RF Car"
-                    description="Frontend development for an AI agent builder, allowing users to create and customize their own AI agents."
-                    image="img/agent-builder.png"
-                    link="https://github.com/AndresOriol/agent_builder_frontend"
+                    description="
+                    Development of a fully 3D printed radiocontrolled car for arm rehabilitation. 
+                    Controled two arduinos and a custom Bluetooth communication protocol.
+                    "
+                    image="img/cars.jpg"
+                    link=""
                     skills={
                         <SkillIconGroup>
-                            <SiReact title="React" className="text-blue-400"/>
-                            <SiJavascript title="JavaScript" className="text-orange-400" />
-                            <SiTailwindcss title="Tailwindcss" className="text-blue-200"/>
-                            <SiVite title="Vite" className="text-blue-900"/>
-                            <SiDocker title="Docker" className="text-blue-600"/>
+                            <SiArduino title="Arduino" className="text-blue-500"/>
+                            <SiC title="C" className="text-blue-300" />
                         </SkillIconGroup>
+                    }
+                    detailedDescription={
+                        <div>
+                            <iframe width="560" height="315" 
+                                src="https://www.youtube.com/embed/5mGxKpf0huA?si=p9rKs7VoDAtfEjl6" 
+                                title="YouTube video player" frameborder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            </iframe>
+                        </div>
                     }
                 />
             </div>
